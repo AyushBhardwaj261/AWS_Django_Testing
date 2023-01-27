debc3c4ae93df138037f28ff6af3740bc754eb67
“AyushBhardwaj321” <ayush.bhardwaj@aiotel.com>
2023-01-27 19:00:05 +0530

    commented django migrate script

--- .platform/hooks/postdeploy/01_django_migrate.sh
+++ .platform/hooks/postdeploy/01_django_migrate.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
-source /var/app/venv/*/bin/activate && {
+# source /var/app/venv/*/bin/activate && {
 
-# collect static
-python manage.py collectstatic --noinput;
-# log which migrations have already been applied
-python manage.py showmigrations;
-# migrate user model
-python manage.py migrate users --noinput;
-# migrate the rest
-python manage.py migrate --noinput;
-}
+# # collect static
+# python manage.py collectstatic --noinput;
+# # log which migrations have already been applied
+# python manage.py showmigrations;
+# # migrate user model
+# python manage.py migrate users --noinput;
+# # migrate the rest
+# python manage.py migrate --noinput;
+# }
